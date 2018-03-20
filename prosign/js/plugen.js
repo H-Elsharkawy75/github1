0beea984eeebcc6e350f666b35b9182a2907458e
H-Elsharkawy75 <eh100@yahoo.com>
2018-03-20 22:38:48 +0200

    prosign important
    prosign

---
+++ prosign/js/plugen.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+   $(".click-edit .fa").click(function()
+    {
+        $(".click-edit .editeee").toggle()   
+    }); 
+    
+    $(".click-ediffdt .fa").click(function()
+    {
+        $(".click-ediffdt .editesaaasee").toggle()   
+    });
+});
+
---
+++ prosign/js/skitter-script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.skitter-large').skitter({
+       numbers: true,
+                dots: false,
+        /*numbers: true,
+        dots: true,
+         controls: true,
+        /*focus: true,*/
+       
+    });
+});
